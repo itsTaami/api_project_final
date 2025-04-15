e10aa8feff3209ed1003c14cc6e856bca1fbd874
itsTaami <115851098+itsTaami@users.noreply.github.com>
2025-04-15 11:30:32 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
-from typing import Optional
-
 from fastapi import FastAPI
+from admin import router as admin_router
+from user import router as user_router
 
 app = FastAPI()
 
+app.include_router(admin_router)
+app.include_router(user_router)
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
